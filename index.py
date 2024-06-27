7cdaade9fa2eb4b2b1be44f86533771cb5909cc8
GROUP5AI <teferabiruk19@gmail.com>
2024-06-27 20:44:29 +0300

    BIRUK COMMITED

--- index.py
+++ index.py
@@ -126,3 +126,17 @@ button = tkinter.Button(frame, text="restart", font=("Consolas", 20), background
 button.grid(row=4, column=0, columnspan=3, sticky="we")
 frame.pack()
 
+#center the window
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+#format "(w)x(h)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+window.mainloop()
